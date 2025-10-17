2fa55f695e9ea6c5122e27be315950b7eda7817b
Isaac A Moore <64980436+isaacnfairplay@users.noreply.github.com>
2025-10-17 12:37:26 -0400

    chore: prepare 1.1.1 release

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ project = "DuckPlus"
 author = "DuckPlus maintainers"
 current_year = _dt.date.today().year
 copyright = f"{current_year}, {author}"
-release = "1.1.0"
+release = "1.1.1"
 version = "1.1"
 
 extensions = [
