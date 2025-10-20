633fff9cef304462178cd27130fc9501650ccb85
Isaac A Moore <64980436+isaacnfairplay@users.noreply.github.com>
2025-10-20 06:12:59 -0400

    Expose aggregate helper barrels

--- duckplus/functions/__init__.py
+++ duckplus/functions/__init__.py
@@ -2,6 +2,41 @@
 
 from __future__ import annotations
 
-from . import aggregate  # noqa: F401  # Import side effects register helpers.
+from . import aggregate
+from .aggregate import (
+    SIDE_EFFECT_MODULES as _AGGREGATE_SIDE_EFFECT_MODULES,
+    approx_count_distinct,
+    approx_count_distinct_filter,
+    approx_quantile_generic,
+    approx_quantile_generic_filter,
+    approx_quantile_numeric,
+    approx_quantile_numeric_filter,
+    approx_top_k,
+    approx_top_k_filter,
+    histogram,
+    histogram_exact,
+    histogram_exact_filter,
+    histogram_filter,
+)
 
-__all__ = ["aggregate"]
+SIDE_EFFECT_MODULES: tuple[str, ...] = (
+    "duckplus.functions.aggregate",
+    *_AGGREGATE_SIDE_EFFECT_MODULES,
+)
+
+__all__ = [
+    "aggregate",
+    "approx_count_distinct",
+    "approx_count_distinct_filter",
+    "approx_quantile_generic",
+    "approx_quantile_generic_filter",
+    "approx_quantile_numeric",
+    "approx_quantile_numeric_filter",
+    "approx_top_k",
+    "approx_top_k_filter",
+    "histogram",
+    "histogram_exact",
+    "histogram_exact_filter",
+    "histogram_filter",
+    "SIDE_EFFECT_MODULES",
+]
